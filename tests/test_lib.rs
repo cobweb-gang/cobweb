378b66a5a8647d648e8ce7a12215a86905aba6f0
David Bernado <dbernado@protonmail.com>
2018-07-13 14:59:13 -0700

    add gitignore

--- tests/test_lib.rs
+++ tests/test_lib.rs
@@ -1,24 +1,8 @@
 extern crate cobweb;
 
 mod test {
-    extern crate mac_address;
-
-    use cobweb::node::get_info;
-    use cobweb::node::Info;
-    use self::mac_address::get_mac_address;
 
     #[test]
-    fn mac_addr() {
-        let info = get_info();
-        let test_info = Info {
-            mac: get_mac_address().unwrap(),
-            source: false,
-            web_connected: false,
-        };
-
-        assert_eq!(
-            test_info.mac,
-            info.mac
-        );
+    fn post_info {
     }
 }
