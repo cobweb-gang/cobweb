f480bb2d9f02498849c5ca440e6c2906eb47d3a2
David Bernado <dbernado@protonmail.com>
2018-07-17 10:46:06 -0700

    implement RPC mechanism

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,32 @@
+extern crate mac_address;
+extern crate fibers;
+extern crate fibers_rpc;
+extern crate mio;
+extern crate futures;
+extern crate serde;
+extern crate bincode;
+extern crate bytecodec;
+#[cfg(feature = "bincode_codec")]
+
 pub mod node {
-    extern crate mac_address;
-    extern crate fibers_rpc;
-    extern crate bytecodec;
-    extern crate fibers;
-    extern crate futures;
+
 
     use std::fmt;
-    use self::mac_address::get_mac_address;
-    use self::futures::{Future, Stream};
-    use self::bytecodec::bytes::{BytesEncoder, RemainingBytesDecoder};
-    use self::fibers::{Executor, InPlaceExecutor, Spawn};
-    use self::fibers_rpc::{Call, ProcedureId};
-    use self::fibers_rpc::client::ClientServiceBuilder;
-    use self::fibers_rpc::server::{HandleCall, Reply, ServerBuilder};
-    use self::futures::Future;
+    use std::net::IpAddr;
+    use ::mac_address::get_mac_address;
+    use ::futures::{Future, Stream};
+    use ::fibers::{Executor, InPlaceExecutor, Spawn};
+    use ::fibers_rpc::{Call, ProcedureId};
+    use ::fibers_rpc::client::ClientServiceBuilder;
+    use ::bytecodec::bytes::{BytesEncoder, RemainingBytesDecoder};
+    use ::bytecodec::bincode_codec::{BincodeEncoder, BincodeDecoder};
+    use ::fibers_rpc::server::{HandleCall, Reply, ServerBuilder};
+    use ::bincode::{serialize, deserialize};
 
     // Types
 
     pub struct Info {
-        mac: mac_address::MacAddress,
+        mac: String,
         node_type: NodeType,
     }
 
@@ -42,12 +50,12 @@ pub mod node {
 
     impl fmt::Display for InfoRes {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            let accept = match *self.accepted {
+            let accept = match self.accepted {
                 true => "ACCEPTED",
                 false => "REJECTED",
             };
             
-            let blacklist = match *self.blacklisted {
+            let blacklist = match self.blacklisted {
                 true => "Your device has been blacklisted.",
                 false => "An error has occurred.",
             };
@@ -99,13 +107,13 @@ pub mod node {
     impl Info {
         pub fn new(type_arg: NodeType) -> Info {
             Info {
-                mac: get_mac_address().unwrap().unwrap(),
+                mac: format!("{}", get_mac_address().unwrap().unwrap()),
                 node_type: type_arg,
             }
         }
     }
 
-    // RPCs and handlers
+    // RPCs
 
     struct InfoRpc;
     impl Call for InfoRpc {
@@ -131,7 +139,7 @@ pub mod node {
     // Functions
 
     pub fn start_rpc_server() {
-        let mut executor = InPlaceExecutor::new()::unwrap();
+        let mut executor = InPlaceExecutor::new().unwrap();
 
         let server_addr = "127.0.0.1:1919".parse().unwrap();
         let server = ServerBuilder::new(server_addr)
@@ -139,12 +147,12 @@ pub mod node {
             .finish(executor.handle());
         executor.spawn(server.map_err(|e| panic!("{}", e)));
     }
-    
-    pub fn validate() -> Result<(), ()> {
-        let mut executor = InPlaceExecutor::new()::unwrap();
+
+    pub fn send_info(server_addr: IpAddr) {
+        let mut executor = InPlaceExecutor::new().unwrap();
         let service = ClientServiceBuilder::new().finish(executor.handle());
 
-        let request = Info::new();
+        let request = Info::new(NodeType::Link).unwrap();
         let response = InfoRpc::client(&service.handle()).call(server_addr, request.clone());
 
         executor.spawn(service.map_err(|e| panic!("{}", e)));
