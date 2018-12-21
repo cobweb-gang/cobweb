72e05b24089a9eda05cfcf65aede348349028f3e
David Bernado <dbernado@protonmail.com>
2018-12-20 20:55:13 -0800

    add error handling for handshake

--- src/sec/client.rs
+++ src/sec/client.rs
@@ -3,19 +3,28 @@ use keybob::{Key, KeyType};
 use tokio_core::net::UdpSocket;
 use std::net::SocketAddr;
 
-pub fn handshake(client_id: &str, server_addr: &SocketAddr, sock: &UdpSocket, pass: &str) -> Key {
+pub fn handshake(client_id: &str, server_addr: &SocketAddr, sock: &UdpSocket, pass: &str) -> Result<Key, String> {
+    let mut error = "";
     let (spake, outbound_msg) = SPAKE2::<Ed25519Group>::start_a(
         &Password::new(pass.as_bytes()),
         &Identity::new(client_id.as_bytes()),
         &Identity::new(format!("{}", server_addr).as_bytes())
         );
 
-    sock.connect(server_addr).unwrap();
+    sock.connect(server_addr).unwrap_or_else(|_| {
+        error = "ERROR: Unable to connect to server";
+    });
     sock.send(outbound_msg.as_slice()).unwrap();
     let inbound_msg: &mut [u8] = &mut [0u8];
     sock.recv(inbound_msg).unwrap();
 
     let key = spake.finish(&inbound_msg).unwrap();
     let key_pass = format!("{:?}", key);
-    Key::from_pw(KeyType::Aes128, &key_pass, client_id)
+
+    let result = match error {
+        "ERROR: Unable to connect to server" => Err(String::from(error)),
+        _ => Ok(Key::from_pw(KeyType::Aes128, &key_pass, client_id)),
+    };
+
+    result
 }
--- src/sec/server.rs
+++ src/sec/server.rs
@@ -1,22 +1,30 @@
 use spake2::{Ed25519Group, Identity, Password, SPAKE2};
 use keybob::{Key, KeyType};
 use tokio_core::net::UdpSocket;
-use std::net::{SocketAddr, SocketAddrV4};
-use std::str::FromStr;
+use std::net::SocketAddr;
 
-pub fn handshake(client_id: &str, server_id: &str, sock: &UdpSocket, pass: &str) -> Key {
+pub fn handshake(client_addr: &SocketAddr, server_id: &str, sock: &UdpSocket, pass: &str) -> Result<Key, String> {
+    let mut error = "";
     let (spake, outbound_msg) = SPAKE2::<Ed25519Group>::start_b(
         &Password::new(pass.as_bytes()),
-        &Identity::new(client_id.as_bytes()),
-        &Identity::new(server_id.as_bytes()));
+        &Identity::new(format!("{}", client_addr).as_bytes()),
+        &Identity::new(server_id.as_bytes()),
+        );
 
-    let client_addr = SocketAddr::from(SocketAddrV4::from_str(client_id).unwrap());
-    sock.connect(&client_addr).unwrap();
+    sock.connect(client_addr).unwrap_or_else(|_| {
+        error = "ERROR: Unable to connect to server";
+    });
     sock.send(outbound_msg.as_slice()).unwrap();
     let inbound_msg: &mut [u8] = &mut [0u8];
     sock.recv(inbound_msg).unwrap();
 
     let key = spake.finish(&inbound_msg).unwrap();
     let key_pass = format!("{:?}", key);
-    Key::from_pw(KeyType::Aes128, &key_pass, client_id)
+
+    let result = match error {
+        "ERROR: Unable to connect to server" => Err(String::from(error)),
+        _ => Ok(Key::from_pw(KeyType::Aes128, &key_pass, server_id)),
+    };
+
+    result
 }
